11d1089575cc991e614776a406a03b991160e788
Blue <azhanging@qq.com>
2019-12-27 18:24:18 +0800

    update

--- apps/blue-rpg-game/src/views/scene/knapsack/Knapsack.vue
+++ apps/blue-rpg-game/src/views/scene/knapsack/Knapsack.vue
@@ -5,23 +5,40 @@
       title: `背包`
     }"/>
     <div class="bc-pd-lr-10rp bc-f-b bc-t-c">
-      <div class="bc-flex bc-flex-jc-c bc-pd-tb-10rp bc-bd-b-e5e bc-t-base" v-for="resource in knapsackList">
-        <div class="bc-flex-1">
-          {{resource.name}}
+      <div>
+        <div class="bc-flex bc-pd-14rp bc-f-b bc-bd-b-e5e">
+          已装备
         </div>
-        <div class="bc-flex-1">
-          x{{resource.amount}}
+        <!-- 已使用装备 -->
+        <div class="bc-flex bc-flex-jc-c bc-pd-tb-10rp bc-bd-b-e5e bc-t-base" v-for="resource in equipmentList">
+          <div class="bc-flex-1">
+            {{resource.name}}
+          </div>
+          <div class="bc-flex-1">
+            卸下
+          </div>
         </div>
-        <div class="bc-flex-1">
-          <a href="javascript:;" class="bc-t-base" v-if="resource.type === 1" @click="useConsume(resource)">
-            使用
-          </a>
-          <template v-if="/^[3]$/.test(resource.type)">
-            <a href="javascript:;" class="bc-t-base" @click="useEquipment(resource)">
+      </div>
+
+      <div>
+        <div class="bc-flex bc-pd-14rp bc-f-b bc-bd-b-e5e">
+          背包
+        </div>
+        <div class="bc-flex bc-flex-jc-c bc-pd-tb-10rp bc-bd-b-e5e bc-t-base" v-for="resource in knapsackList">
+          <div class="bc-flex-1">
+            {{resource.name}}
+          </div>
+          <div class="bc-flex-1">
+            x{{resource.amount}}
+          </div>
+          <div class="bc-flex-1">
+            <a href="javascript:;" class="bc-t-base" v-if="resource.type === 1" @click="useConsume(resource)">
+              使用
+            </a>
+            <a href="javascript:;" class="bc-t-base" @click="useEquipment(resource)" v-if="/^[3]$/.test(resource.type)">
               装备
             </a>
-          </template>
-
+          </div>
         </div>
       </div>
     </div>
@@ -33,20 +50,31 @@
     name: "knapsack",
     data() {
       return {
+        //装备列表
+        equipmentList: [],
         //背包列表
         knapsackList: []
       };
     },
     created() {
+      this.getEquipment();
       this.getKnapsack();
     },
     methods: {
+      //获取装备列表
+      getEquipment() {
+        this.$axios.get('/member/scene/knapsack/equipment').then((res) => {
+          const { data } = res;
+          this.equipmentList = data.list;
+        });
+      },
       getKnapsack() {
         this.$axios.get(`/member/scene/knapsack/resource`).then((res) => {
           const { data } = res;
           this.knapsackList = data.list;
         });
-      },
+      }
+      ,
       //使用消耗品
       useConsume(resource) {
         this.$axios.get(`/member/scene/knapsack/useConsume`, {
@@ -60,12 +88,13 @@
 
       //使用装备
       useEquipment(resource) {
-        this.$axios.get(`/member/scene/knapsack/useEquipment`,{
+        this.$axios.get(`/member/scene/knapsack/useEquipment`, {
           params: {
             id: resource.id
           }
         }).then(() => {
-
+          this.getEquipment();
+          this.getKnapsack();
         });
       }
     }
